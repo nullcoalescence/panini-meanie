1064844b8cfa44fa6d1c15815fbdaaabedfcca3d
ben <{username}@users.noreply.github.com>
2023-04-29 01:59:23 -0500

    added OrderController, MenuController

---
+++ panini-meanie-api/Controllers/MenuController.cs
@@ -0,0 +1,26 @@
+﻿// MenuController.cs
+//
+// Api controller for the menu portion of the app
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace panini_meanie_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MenuController : ControllerBase
+    {
+        private readonly ILogger<MenuController> _logger;
+
+        public MenuController(ILogger<MenuController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet, Route("get")]
+        public IEnumerable<string> Get()
+        {
+            return new List<string>() { "supper" };
+        }
+    }
+}
